9cc5f487e1da5fd677eaeef4c545d7336ccbba61
Lux ʾ ☽☾ <alinerayssa39@icloud.com>
2025-09-22 10:46:44 -0300

    Add files via upload

---
+++ AULA 1.js
@@ -0,0 +1,17 @@
+alert("boas vindas ao nosso site!")
+ let Nome = "Lua";
+ let idade = 25;
+ let numeroDeVendas = 50;
+ let saldoDisponivel = 1000;
+
+ alert("Erro! Preencher todos os campos");
+
+ let mensagemDeErro = "Erro! Preencha todos os campos";
+ alert(mensagemDeErro);
+
+ Nome = prompt("Digite seu nome:");
+ idade = prompt("Digite sua idade:")
+ if(idade >= 18) {
+    alert("Pode tirar a habilitação");
+
+ }
---
+++ AULA 2.js
@@ -0,0 +1,12 @@
+alert("boas vindas ao nosso site!")
+ let numeroSecreto = 22;
+ console.log(numeroSecreto)
+ let chute = prompt('Escolha um número entre 0 e 30')
+
+ if(chute == numeroSecreto) {
+   alert(`Isso aí! Descobriu o número secreto ${numeroSecreto}`);
+ } else {
+   alert('Você errou')
+ }
+
+ 
