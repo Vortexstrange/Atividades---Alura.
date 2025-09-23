2eaa5968ff8fca54e1708d3ae346cbc27bcfd148
Lux ʾ ☽☾ <alinerayssa39@icloud.com>
2025-09-23 11:45:53 -0300

    AULA 3 - exerc. 2
    Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.

---
+++ AULA 3 - exerc. 2.js
@@ -0,0 +1,9 @@
+let numero = prompt ("Digite um número");
+
+if (numero < 0) {
+    alert(" O número escolhido é Negativo!");
+} else if (numero > 0) {
+    alert("O número escolhido é Positivo");
+} else {
+    alert("O número escolhido é Neutro")
+}
